7bf15b36a77ab13377728676300974dc9cbf46be
AnassALOUANE <anass.alouane@gmail.com>
2021-10-27 17:57:16 +0100

    remove unused code

--- Tests/YouCanPayTests/YouCanPayTests.swift
+++ Tests/YouCanPayTests/YouCanPayTests.swift
@@ -21,25 +21,6 @@ final class YouCanPayTests: XCTestCase {
         try super.tearDownWithError()
     }
     
-    func testValidatingCard() {
-        let promise = expectation(description: "date correct")
-        let cardInfo = YCPCardInformation(cardHolderName: "anass", cardNumber: "5487477802835935", expiryDate: "10/23", cvv: "555")
-        do{
-        try YCPCardValidation.validate(cardInfo)
-            promise.fulfill()
-        }catch{
-            print("error: \(error.localizedDescription)")
-        }
-        
-        wait(for: [promise], timeout: 2)
-    }
-    
-    func testLocale() {
-        let string = YCPLocalizable.get("An error occurred while processing the payment.")
-        
-        XCTAssertEqual(string, "An error occurred while processing the payment.")
-    }
-    
     // testing payment with a none 3DS card -> Succeeded
     func testPayEndpointNone3dsSucceeded() throws {
         // given
