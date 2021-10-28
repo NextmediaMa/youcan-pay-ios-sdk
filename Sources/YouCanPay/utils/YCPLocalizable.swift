05839456e80350d6f2350a64fa98097eaeeb8ad8
AnassALOUANE <anass.alouane@gmail.com>
2021-10-28 10:25:00 +0100

    update spec

--- Sources/YouCanPay/utils/YCPLocalizable.swift
+++ Sources/YouCanPay/utils/YCPLocalizable.swift
@@ -17,7 +17,7 @@ extension Foundation.Bundle {
         /// Returns the resource bundle associated with the current Swift module.
         static var current: Bundle = {
                 // This is your `target.path` (located in your `Package.swift`) by replacing all the `/` by the `_`.
-                let bundleName = "Sources"
+                let bundleName = "YouCanPay_YouCanPay"
                 let candidates = [
                         // Bundle should be present here when the package is linked into an App.
                         Bundle.main.resourceURL,
